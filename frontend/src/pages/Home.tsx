ae22225af782ebfa62a9820724496fa233a40381
merontekle27 <merytek30@gmail.com>
2026-05-03 20:38:09 +0300

    feat: implement CityHighlightSection and integrate i18n configuration into Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,11 +4,13 @@ import LeadershipSection from '../components/home/LeadershipSection';
 import ExploreSection from '../components/home/ExploreSection';
 import ServicesHubSection from '../components/home/ServicesHubSection';
 import MobileAppSection from '../components/home/MobileAppSection';
+import CityHighlightSection from '../components/home/CityHighlightSection';
 
 const Home = () => {
   return (
     <div className="bg-slate-50/50 relative">
       <HeroSection />
+      <CityHighlightSection />
       <CityFlashCards />
       <LeadershipSection />
       <ExploreSection />
